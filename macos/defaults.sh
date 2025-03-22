5b7e1fb73af7611f724a68205a388cb473dc7dd2
ternera <hi@ternera.org>
2025-03-21 21:03:56 -0500

    change backlight timeout to 5s instead of 5m

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -73,8 +73,8 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15
 # Automatically illuminate built-in MacBook keyboard in low light
 defaults write com.apple.BezelServices kDim -bool true
 
-# Turn off keyboard illumination when computer is not used for 5 minutes
-#defaults write com.apple.BezelServices kDimTime -int 300
+# Turn off keyboard illumination when computer is not used for 5 seconds
+defaults write com.apple.BezelServices kDimTime -int 5
 
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
